53819b00118d26be4c5784016aa0ff1726ea67ee
Neider1910 <neim1910@gmail.com>
2020-10-19 15:21:00 -0500

    full
    Signed-off-by: Neider1910 <neim1910@gmail.com>

--- ApartmentApi/index.js
+++ ApartmentApi/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors= require("cors");
 
 const apiRouter = require("./routes/api");
 const app = express();
 require("./db");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 //peticiones de api
-app.use("/api", apiRouter);
+app.use("/api", apiRouter)
 //llamado
 app.listen(3001, () => {
   console.log("servidor arrancando!");
